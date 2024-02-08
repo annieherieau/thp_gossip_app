81fcfebac08b00541d30d8d41f6aed76908a7553
annieherieau <annie.herieau@gmail.com>
2024-02-08 22:52:17 +0100

    seed fix users

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ City.destroy_all
 User.destroy_all
 text_array = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices neque ornare aenean euismod elementum. Potenti nullam ac tortor vitae purus faucibus. Aliquet sagittis id consectetur purus ut faucibus pulvinar. Id ornare arcu odio ut sem nulla pharetra diam. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu. Lorem sed risus ultricies tristique nulla aliquet enim tortor at. Fames ac turpis egestas integer eget aliquet nibh praesent. In pellentesque massa placerat duis ultricies lacus. Donec massa sapien faucibus et molestie ac feugiat. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est. Ac odio tempor orci dapibus. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec.".split('. ')
 
-# création des cities
 10.times do |i|
   City.create!(
     zip_code: Faker::Address.zip_code.split('-').first,
     name: Faker::Address.city 
   )
 end
+puts '--- 10 cities ---'
 
 10.times do |i|
   User.create!(
@@ -33,4 +33,7 @@ end
     city: City.all.sample
   )
 end
+puts '--- 10 users ---'
+
+
 
